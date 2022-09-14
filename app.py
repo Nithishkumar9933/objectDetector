69581d1f3d078cc65bdab7aa859e52d6f8b595d3
Nithishkumar9933 <Nithinkumar9933@gmail.com>
2022-09-14 23:30:13 +0530

    indentation

--- app.py
+++ app.py
@@ -61,6 +61,7 @@ def upload():
                                 filename=filename))
         
 @app.route('/uploads/<filename>')
+
 def uploaded_file(filename):
     PATH_TO_TEST_IMAGES_DIR = app.config['UPLOAD_FOLDER']
     TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR,filename.format(i)) for i in range(1, 2) ]
@@ -93,9 +94,6 @@ def uploaded_file(filename):
 
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
